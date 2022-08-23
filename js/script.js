561de8d8644f655a564df5e502385e72d4216396
Mariana Muñoz <mariana412@gmail.com>
2022-08-22 20:17:42 -0500

    New

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function eliminaCookie() {
+    var elementoCookie = document.querySelector('.cookie'); 
+    elementoCookie.remove();
+}
+
+function car(car1) {
+
+    alert("Your car is empty");
+
+}
+
+function change() {
+    document.getElementById("chg").src="images/succulents-2.jpg"
+}
+
+function change2() {
+    document.getElementById("chg").src="images/succulents-1.jpg"
+}
